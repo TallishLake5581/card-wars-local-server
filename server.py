5262c9a244a2d419a11b895f47c010a0e7c9e07d
TallishLake5581 <134895708+TallishLake5581@users.noreply.github.com>
2026-07-25 16:37:18 +0400

    server.py

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, request
+from flask import send_from_directory, jsonify
 
 app = Flask(__name__)
-
-
-@app.route("/persist/static/<path:filename>")
-def serve_static(filename):
-  return send_from_directory("static", filename)
 
 
 @app.route("/")
 def home():
   return "Server is Running!", 200
+
+
+@app.route("/persist/static/<path:filename>")
+def get_file(filename):
+  print(f"File requested: {filename}")
+  return send_from_directory("persist/static", filename)
+
+
+@app.errorhandler(404)
+def not_found(e):
+  print(f"Missing path: {request.path}")
+  return jsonify({"status": "error"}), 404
 
 
 if __name__ == "__main__":
